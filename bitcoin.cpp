d198f663c0d907582d4f7cce4e958fe323864402
Marlon Hanks <corporate@kedia.us>
2014-10-01 02:47:25 -0400

    Update bitcoin.cpp

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/litecoin-seeder:0.01/";
+    string ver = "/tetcoin-seeder:0.01/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
@@ -296,7 +296,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 /*
 int main(void) {
-  CService ip("litecointools.com", 9333, true);
+  CService ip("tetcoin.com", 8888, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
