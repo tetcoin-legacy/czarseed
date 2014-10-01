f678d749c6632f13e06cc7a201ff8314024bdc1a
Marlon Hanks <corporate@kedia.us>
2014-10-01 02:50:57 -0400

    Update db.h

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70002
+#define REQUIRE_VERSION 70003
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 470000;
+    return testnet ? 0 : 60000;
 }
 
 std::string static inline ToString(const CService &ip) {
